b7b5894c6e4f2125d700b3311f34f35541f04d39
Ilyas <ilyas.sultanov2020@list.ru>
2021-09-13 13:37:19 +0500

    .

---
+++ js/db.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB_IIguy-RWL-BjxDCm896nxS09yrej59A",
+    authDomain: "olx-clone-632ce.firebaseapp.com",
+    projectId: "olx-clone-632ce",
+    storageBucket: "olx-clone-632ce.appspot.com",
+    messagingSenderId: "440158144650",
+    appId: "1:440158144650:web:509e934dacbcce113d0573",
+    measurementId: "G-5SW97Q0FKE"
+};
+const init = firebase.initializeApp(firebaseConfig);
+
+export default {
+    firebaseConfig,
+    init
+}
---
+++ js/main.js
@@ -0,0 +1,2 @@
+import DB from './db.js';
+DB
